8cdfb4abe0823865f45aa764bb8e281446f89514
dom summers <summers8734@student.cptc.edu>
2021-03-07 12:33:16 -0800

    Added Note abt where to use Session and Startup

--- eCommerceSite/Startup.cs
+++ eCommerceSite/Startup.cs
@@ -59,6 +59,7 @@ namespace eCommerceSite
 
             app.UseAuthorization();
 
+            // Must be between UseRouting() and UseEndPoints()
             app.UseSession();
 
             app.UseEndpoints(endpoints =>
