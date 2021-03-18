3e5ff0f2c426ec2b35432100f99e6297690f907a
doms92 <72600993+doms92@users.noreply.github.com>
2021-03-18 14:59:09 -0700

    Issue#34 cart add item success (#38)
    * Added sucess message when item is added to Cart
    
    * Redirect to previous page when adding a product to cart

--- eCommerceSite/Controllers/CartController.cs
+++ eCommerceSite/Controllers/CartController.cs
@@ -21,14 +21,16 @@ namespace eCommerceSite.Controllers
             _httpContext = httpContext;
         }
 
-        public async Task<IActionResult> Add(int id)
+        public async Task<IActionResult> Add(int id, string prevUrl)
         {
             Product p = await ProductDb.GetProductAsync(_context, id);
 
             CookieHelper.AddProductToCart(_httpContext, p);
 
+            TempData["Message"] = p.Title +  " added successfully";
+
             // Redirect back to previous page
-            return RedirectToAction("Index", "Product");
+            return Redirect(prevUrl);
         }
 
         public IActionResult Summary()
