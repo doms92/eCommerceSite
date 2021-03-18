7e3d2f87177fa742af0ede5335badb3c1eef73a3
dom summers <summers8734@student.cptc.edu>
2021-03-17 17:46:55 -0700

    Refactored Cart Controller

--- eCommerceSite/Controllers/CartController.cs
+++ eCommerceSite/Controllers/CartController.cs
@@ -21,35 +21,11 @@ namespace eCommerceSite.Controllers
             _httpContext = httpContext;
         }
 
-        public async Task<IActionResult> AddAsync(int id)
+        public async Task<IActionResult> Add(int id)
         {
             Product p = await ProductDb.GetProductAsync(_context, id);
 
-            const string CartCookie = "CartCookie";
-
-            // Get exisiting cart items
-            string existingItems =
-             _httpContext.HttpContext.Request.Cookies[CartCookie];
-
-            List<Product> cartProducts = new List<Product>();
-            if (existingItems != null)
-            {
-                cartProducts =
-                    JsonConvert.DeserializeObject<List<Product>>(existingItems);
-            }
-            // Add current product to exisiting cart
-
-            // Add product list to cart cookie
-            string data = JsonConvert.SerializeObject(cartProducts);
-
-            CookieOptions options = new CookieOptions()
-            {
-                Expires = DateTime.Now.AddYears(1),
-                Secure = true,
-                IsEssential = true
-            };
-
-            _httpContext.HttpContext.Response.Cookies.Append(CartCookie, data, options);
+            CookieHelper.AddProductToCart(_httpContext, p);
 
             // Redirect back to previous page
             return RedirectToAction("Index", "Product");
@@ -57,11 +33,8 @@ namespace eCommerceSite.Controllers
 
         public IActionResult Summary()
         {
-            string cookieData = _httpContext.HttpContext.Request.Cookies["CartCookie"];
-
-            List<Product> cartProducts = JsonConvert.DeserializeObject<List<Product>>(cookieData);
-
-            return View(cartProducts);
+            
+            return View(CookieHelper.GetCartProducts(_httpContext));
         }
     }
 }
